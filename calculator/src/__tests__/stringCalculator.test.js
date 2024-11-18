20a8b5948bafc544202449cb82126b6c6c219113
savitha dhatchinamoorthy <savithadatchu@gmail.com>
2024-11-18 23:02:29 +0530

    basic addition with delimiters added, newline testcase added

--- calculator/src/__tests__/stringCalculator.test.js
+++ calculator/src/__tests__/stringCalculator.test.js
@@ -4,4 +4,21 @@ describe('String Calculator', () => {
   it('should return 0 if input is an empty string', () => {
     expect(add('')).toBe(0);
   });
+
+  it('should return the number if input string is a single number', () => {
+    expect(add('1')).toBe(1);
+  });
+
+  it('should handle two numbers with default delimiter', () => {
+    expect(add('1,2')).toBe(3);
+  });
+
+  it('should handle multiple numbers with default delimiter', () => {
+    expect(add('1,2,3')).toBe(6);
+  });
+
+  it('should handle multiple numbers with \n and default delimiter', () => {
+    expect(add('1\n2,3')).toBe(6);
+  });
+
 });
--- calculator/src/utils/stringCalculator.js
+++ calculator/src/utils/stringCalculator.js
@@ -1,4 +1,6 @@
 export function add(numbers) {
+    //return 0 for empty string
     if (numbers === "") return 0;
-}
+    return numbers.split(',').reduce((sum, num) => sum + Number(num), 0);
+  }
   
